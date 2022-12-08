c2959e3a59447e0b9ead90f330a7d8bfb20199dc
SekoVper <damehlivingstone1@gmail.com>
2022-12-08 16:02:52 +0100

    Modify Blogs file

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,15 +1,27 @@
 const Blogs = (props) => {
-  const blogs = props.blogs;
-  return ( 
-    <div className="blog-list">
-      {blogs.map(blog => (
-      <div className="blog-preview" key={blog.id}>
-        <h2>{blog.title}</h2>
-        <p>By {blog.author}</p>
+  const { blogs } = props;
+  const { title } = props;
+  const { deleteBlog } = props;
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <div className="blog-list">
+        {blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+            <p>
+              By
+              {blog.author}
+            </p>
+            <button type="button" onClick={() => deleteBlog(blog.id)}>Delete</button>
+          </div>
+        ))}
       </div>
-    ))}
-    </div>
-   );
-}
- 
+
+    </>
+  );
+};
+
 export default Blogs;

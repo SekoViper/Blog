4495a236088feedc13dfc04eb3aa6c8aecac133e
SekoVper <damehlivingstone1@gmail.com>
2022-12-08 16:09:56 +0100

    Modify Home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import { React, useState } from 'react';
 import Blogs from './Blogs';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    {
+      title: 'My new website', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sapiente corporis ipsam expedita illum soluta consequuntur sit suscipit! Ipsa, fugit?', author: ' mario', id: 1,
+    },
+    {
+      title: 'Welcome party!', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sapiente corporis ipsam expedita illum soluta consequuntur sit suscipit! Ipsa, fugit?', author: ' yoshi', id: 2,
+    },
+    {
+      title: 'Web dev top tips', body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sapiente corporis ipsam expedita illum soluta consequuntur sit suscipit! Ipsa, fugit?', author: ' mario', id: 3,
+    },
   ]);
 
-  return ( 
+  const deleteBlog = (id) => {
+    const filteredBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(filteredBlogs);
+  };
+
+  return (
     <div className="home">
-      <Blogs blogs={blogs}/>
+      <Blogs blogs={blogs} title="Our Blogs" deleteBlog={deleteBlog} />
+      <Blogs blogs={blogs.filter((blog) => blog.author === ' yoshi')} title="Yoshi's Blog" deleteBlog={deleteBlog} />
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;

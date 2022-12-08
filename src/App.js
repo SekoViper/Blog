943d4dee750c6384f33cabebc3970e0dc67ab507
SekoVper <damehlivingstone1@gmail.com>
2022-12-08 15:48:06 +0100

    Modify the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
-
 function App() {
   return (
     <div className="App">
-        <Navbar />
-      <div className='content'>
+      <Navbar />
+      <div className="content">
         <Home />
       </div>
     </div>

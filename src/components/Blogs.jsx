cb2a274cc17d75ce1e8dc367ba36f755c4c0c81a
SekoVper <damehlivingstone1@gmail.com>
2022-12-09 19:12:17 +0100

    Add components for the app

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Blogs = ({ blogs, title, deleteBlog }) => (
+  <>
+    <h1>{title}</h1>
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+          <p>
+            By
+            {blog.author}
+          </p>
+          <button type="button" onClick={() => deleteBlog(blog.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+
+  </>
+);
+Blogs.defaultProps = { blogs: null, title: null, deleteBlog: null };
+Blogs.propTypes = { blogs: PropTypes.string, title: PropTypes.string, deleteBlog: PropTypes.func };
+
+export default Blogs;

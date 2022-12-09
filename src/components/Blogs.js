8f37167d802c0881dc3cf6e697641b4603311520
SekoVper <damehlivingstone1@gmail.com>
2022-12-09 14:27:35 +0100

    Fix props destructing and proptypes error

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,27 +1,25 @@
-const Blogs = (props) => {
-  const { blogs } = props;
-  const { title } = props;
-  const { deleteBlog } = props;
+import PropTypes from 'prop-types';
 
-  return (
-    <>
-      <h1>{title}</h1>
-      <div className="blog-list">
-        {blogs.map((blog) => (
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>{blog.body}</p>
-            <p>
-              By
-              {blog.author}
-            </p>
-            <button type="button" onClick={() => deleteBlog(blog.id)}>Delete</button>
-          </div>
-        ))}
-      </div>
+const Blogs = ({ blogs, title, deleteBlog }) => (
+  <>
+    <h1>{title}</h1>
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.body}</p>
+          <p>
+            By
+            {blog.author}
+          </p>
+          <button type="button" onClick={() => deleteBlog(blog.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
 
-    </>
-  );
-};
+  </>
+);
+Blogs.defaultProps = { blogs: null, title: null, deleteBlog: null };
+Blogs.propTypes = { blogs: PropTypes.string, title: PropTypes.string, deleteBlog: PropTypes.func };
 
 export default Blogs;
